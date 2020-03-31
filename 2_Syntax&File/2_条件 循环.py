c033a25ad4f668489353799c58954dbead9adda6
liuxiaobing13 <liuxiaobing13@jd.com>
2020-03-31 14:31:54 +0800

    if else while for

--- 2_Syntax&File/2_条件 循环.py
+++ 2_Syntax&File/2_条件 循环.py
@@ -49,6 +49,21 @@ while n < len(L):
     n += 1
     pass
 
+strings = ['Hsjhs', 'sdhjngf', 'ssdXXXd', 'sss']
+index = 0
+for string in strings:
+    if 'XXX' in string:
+        strings[index] = '[censored]'
+    index += 1
+else:
+    print(strings)
+
+for index, string in enumerate(strings):
+    if 'ss' in string:
+        strings[index] = 'xxxx{}'.format(index)
+else:
+    print(strings)
+
 # break 可以使循环提前 退出 
 # continue 跳过当前循环
 
@@ -61,3 +76,52 @@ print('Hello, {}!'.format(name))
 iterateDic = {'x': 1, 'y': 2, 'z': 3}
 for key in iterateDic:
     print(key, 'corresponds to', iterateDic[key])
+
+
+# while True/break 
+# 不够优雅的方式
+# word = 'dummy'
+# while word:
+#     word = input('please enter a  word: ')
+#     print('The word was', word)
+
+while True:
+    word = input('please enter a  word: ')
+    if not word: break
+    print('The word was', word)
+
+from math import sqrt
+
+# 可以在循环中添加 else 字句，该else仅在没有调用break时才执行；
+for n in range(99, 81, -1):
+    root = sqrt(n)
+    if root == int(root):
+        print(n)
+        break
+else:
+    # 仅仅没有调用break退出时才会执行
+    print("Didn't find it")
+
+# 列表推导 列表退到是一种从其他列表创建列表的方式，类似于-数学中的集合推导
+# 例：10以内的平方值：
+newList = [x * x for x in range(10)]
+print(newList)
+# 例：10以内可以被3整除的平方值
+newList = [x * x for x in range(10) if x % 3 == 0]
+print(newList)
+
+newList = [(x, y) for x in range(3) for y in range(2, 5)]
+print(newList)
+
+# 例：将名字首字母相同的男孩跟女孩配对 
+# 注：这种方式遍历并检查所有情况是否满足要求，效率不高
+girls = ['alice', 'bernice', 'clarice']
+boys = ['chris', 'arnol', 'bob', 'kris']
+print([b+'+'+g for b in boys for g in girls if b[0] == g[0]])
+
+letterGirls = {}
+for g in girls:
+    letterGirls.setdefault(g[0], []).append(g)
+# print([b+'+'+g for b in boys for g in letterGirls[b[0]] if b[0] in letterGirls])  # 此种写法会奔溃，因为 'k' 在letterGirls 中不存在
+print([b+'+'+g for b in boys if b[0] in letterGirls for g in letterGirls[b[0]]])  # 应该把 if 放在 for 前面
+
