cf13c95e0cf78ce0f58ef392b0e8934f267dc785
Liuxiaobing <liuxiaobing13@jd.com>
2020-04-24 13:59:46 +0800

    模块

---
+++ 10_模块/3import.py
@@ -0,0 +1,5 @@
+import hello_module as hello
+'''
+    模块在首次导入程序时执行, 如：导入hello_module ，运行程序，没有任何操作也会执行hello_module中的内容。这时候需要使用__name__变量，参见hello_module
+'''
+
--- 10_模块/hello_module.py
+++ 10_模块/hello_module.py
@@ -1,4 +1,31 @@
+# -*- coding: utf-8
+# 中文注释或者文档报错的时候，在python 顶部添加上面一行  -*- coding: utf-8，编码
+"""
+    this is a module
+    show is method 
+    testWhileMain and test is test case method
+"""
+
+# this is module name
+''' this is module name '''
+moduleName = 'show hello'
+
 def show():
+    '''show module info'''
     print("hello world, I am a custom module")
 
-print("hello module imported")
+def test():
+    '''hello_module excuted as module'''
+    show()
+    print("hello_module excuted as module")
+
+def testWhileMain():
+    '''hello_module excuted as main'''
+    show()
+    print("hello_module excuted as main")
+
+
+if __name__ == '__main__':
+    testWhileMain()
+else:
+    test()
